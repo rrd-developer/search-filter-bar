421db1062ac5815c81731e9b3ed43d318dc328c3
DHINAKAR R <123221939+rrd-developer@users.noreply.github.com>
2023-08-26 12:21:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import JSONDATA from "./MOCK_DATA.json"
+import { useState } from 'react';
+
+function App() {
+  const [searchItem,setSearchItem] = useState("");
+  return (
+    <div className="App" >
+      <input type='text' placeholder='SEARCH...' onChange={(e)=> setSearchItem(e.target.value)}/>
+
+
+
+
+      {JSONDATA.filter((val)=>{
+        if(searchItem == ""){
+          return val;
+        }else if(val.first_name.toLowerCase().includes(searchItem.toLowerCase())){
+          return val
+        }
+      }).map((val,key)=>{
+       return(
+        <div className='user' key={key}>
+          <p>
+          {val.first_name}
+          </p>
+          </div>
+       )
+      })}
+     
+    </div>
+  );
+}
+
+export default App;
